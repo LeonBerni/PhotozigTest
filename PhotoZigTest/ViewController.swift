7b2746dd17bf813b97eae0acc78ac311061f8a7b
Leon Berni <leonberni@hotmail.com>
2017-11-30 16:20:16 -0200

    Initial Commit

---
+++ PhotoZigTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoZigTest
+//
+//  Created by Leon Berni on 30/11/17.
+//  Copyright © 2017 Leon Berni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
